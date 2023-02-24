1d06fc28ae55439be0ce8c8fd4096243ec2434ab
vitucelestino <victorcelestino13@gmail.com>
2023-02-24 15:09:43 -0300

    Alteração 01

---
+++ Atividade 01/script.js
@@ -0,0 +1,43 @@
+// Atividade - Javascript: Variáveis
+
+// Exercício 01
+// Faça um programa que exiba um decimal, um inteiro e uma utilizando o console.log();
+
+let a = 12.5
+let b = 12
+let c = "Kenzie Academy"
+console.log(a, b, c)
+
+// Exercício 02
+// Declare uma variável, atribua um número, use o método typeof para mostrar na tela a palavra "number".
+
+let variable = 13;
+console.log(typeof variable)
+
+// Exercício 03
+// Declare uma variável, atribua um valor booleano, use o método typeof para mostrar na tela a palavra "boolean".
+
+let booleanValue = true;
+console.log(typeof booleanValue)
+
+// Exercício 04
+// Declare uma variável, atribua uma string, use o método typeof para mostrar na tela a palavra "string".
+
+let stringValue = 'Kenzie Academy';
+console.log(typeof stringValue)
+
+// Exercício 05
+// Faça um programa que exiba duas variáveis, uma com atribuição true e outra false;
+let variableTrue = true;
+let variableFalse = false;
+console.log(variableTrue, variableFalse)
+
+// Exercício 06
+// Usuando uma variável, e o console.log(), mostre no console o valor "null".
+let variableNull = null;
+console.log(variableNull)
+
+// Exercício 07
+// Usando uma variável, e o console.log(), mostre no console o valor 'undefined'.
+let variableNotDefined
+console.log(variableNotDefined)
